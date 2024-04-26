5ba4af446b58aa1363ec0ac44c80c77f5f89b432
Matelz <49626198+Matelz@users.noreply.github.com>
2024-04-25 23:16:49 -0300

    feat: add level load method [no ci]

--- Levels/level_0.py
+++ Levels/level_0.py
@@ -1,10 +1,26 @@
+import pygame
 from Levels.level import Level
 
 
 class Level0(Level):
+    def load(self):
+        self.pos = 0
+
+        self.images = {
+            "background": pygame.image.load("./resources/background.png").convert(),
+        }
+
+        # Check if all images have been loaded successfully
+        self.is_loaded = all(image is not None for image in self.images.values())
+
     def run(self):
-        self.screen.fill("red")
-        print("blip")
+        self.screen.fill("black")
+        self.screen.blit(self.images["background"], (self.pos,0))
+        self.screen.blit(self.images["background"], (self.pos - self.screen.get_width(),0))
+        self.pos += 1
+
+        if self.pos >= self.screen.get_width():
+            self.pos = 0
 
     def get_status(self):
         return self.is_active
--- Levels/level_1.py
+++ Levels/level_1.py
@@ -2,8 +2,11 @@ from Levels.level import Level
 
 
 class Level1(Level):
+    def load(self):
+        pass
+
     def run(self):
-        self.screen.fill("blue")
+        self.screen.fill("red")
 
     def get_status(self):
         return self.is_active
